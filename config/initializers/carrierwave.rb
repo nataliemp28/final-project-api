901a24ba9c0aafcdd85d566dc2de12f9fe3c9224
Natalie Pateman <nataliempateman@gmail.com>
2016-12-09 16:58:10 +0000

    Fixed bug in carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
 
   if Rails.env.production?
     config.root = Rails.root.join('tmp')
-    config.cache_dir = "#{Rails.route}/temp/uploads"
+    config.cache_dir = "#{Rails.root}/temp/uploads"
   end
 
   config.fog_directory = ENV['P4_AWS_BUCKET_NAME']
